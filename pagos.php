20e65be32b0e92ed258698da32408a24dc8977c9
BlackStone94 <eroa94@gmail.com>
2020-11-11 15:57:21 -0600

    423

--- pagos.php
+++ pagos.php
@@ -1,12 +1,16 @@
 <?php
-    if(isset($_POST['producto'],$_POST['precio'])){
-        exit('Ocurrio un error');
+    use Payal\Api\Payer;  
+    if(!isset($_POST['producto'], $_POST['precio'])){
+        exit('Ocurrio un error hola ');
     }
+    require 'configuracion.php';
 
-require 'configuracion.php';
-$producto=htmlspecialchars($_POST['producto']);
-$precio=htmlspecialchars($_POST['precio']);
-$precio=(int)$precio;
-$envio=0;
-$total=$precio+$envio;
-   
+    $producto=htmlspecialchars($_POST['producto']);
+    $precio=htmlspecialchars($_POST['precio']);
+
+    $precio= (int) $precio;
+    $envio=0;
+    $total=$precio+$envio;
+
+    $compra = new Payer();   
+    $compra->setPaymentMethod('paypal');
